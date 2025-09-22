5f975288f228a4dbbc2e02422e13f1b57565042d
lenonbr <lenon.br@hotmail.com>
2025-09-22 16:13:36 -0300

    configurando server.ts

---
+++ src/http/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+    return "Eaeeeeeee";
+});
+
+app.listen({
+    port: 3333,
+    host: "0.0.0.0",
+}).then(() => {
+    console.log("HTTP server running!");
+});
