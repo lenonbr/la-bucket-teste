47470021c11596eae5e9e9763ec03c903ff5c099
lenonbr <lenon.br@hotmail.com>
2025-09-22 16:02:22 -0300

    adiciona demais arquivos do projeto

---
+++ prisma/migrations/20250922185610_create_files/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "public"."File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
