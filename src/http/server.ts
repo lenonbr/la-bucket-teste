4e16bd66e2ef53e6cb4043f4744b4d92bdc58af9
lenonbr <lenon.br@hotmail.com>
2025-09-22 16:52:31 -0300

    criando rota de upload arquivo

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,22 @@
 import fastify from "fastify";
+import {PutObjectCommand} from "@aws-sdk/client-s3";
+import {r2} from "../lib/cloudflare";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 
 const app = fastify();
 
-app.get("/", () => {
-    return "Eaeeeeeee";
+app.post("/uploads", async () => {
+    const signedUrl = await getSignedUrl(
+        r2,
+        new PutObjectCommand({
+            Bucket: "lenon-bucket-teste",
+            Key: "file.mp4",
+            ContentType: "video/mp4",
+        }),
+        {expiresIn: 600}
+    );
+
+    return signedUrl;
 });
 
 app.listen({
--- src/lib/cloudflare.ts
+++ src/lib/cloudflare.ts
@@ -1,4 +1,5 @@
 import {S3Client} from "@aws-sdk/client-s3";
+import {env} from "../env";
 
 export const r2 = new S3Client({
     region: "auto",
